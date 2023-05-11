02b3777f799f0b78ad88563dce5d1144f8199cbf
MohandMagdy <92790188+Mohandmagdy@users.noreply.github.com>
2023-05-11 10:39:11 +0300

    Update customer.java

--- customer.java
+++ customer.java
@@ -64,7 +64,7 @@ public class customer extends user{
     }
 
     void ShowData() {
-        System.out.println("Name: " + this.FullName + "\nEmail: " + this.Email + "\nPhone " + this.phone);
+        System.out.println("Name:" + this.FullName + "\nEmail:" + this.Email + "\nPhone:" + this.phone);
     }
 
 
