c330581757ace41a05a89bb7511184fa8ddf561f
MohandMagdy <92790188+Mohandmagdy@users.noreply.github.com>
2023-05-11 14:06:19 +0300

    Update Toffee.java

--- Toffee.java
+++ Toffee.java
@@ -1,8 +1,12 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Toffee {
     Catalog catalog;
@@ -110,6 +114,119 @@ public class Toffee {
                     break;
                 }
             }
+        }
+    }
+
+    public void clearfile(String f){
+        try {
+            FileWriter fileWriter = new FileWriter(f, false);
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while clearing the file.");
+            e.printStackTrace();
+        }
+    }
+
+    public void write_to_file(String f){
+        try {
+            FileWriter fileWriter = new FileWriter(f);
+            for(customer c: customers){
+                fileWriter.write(c.ID+"\n");
+                fileWriter.write(c.Email+"\n");
+                fileWriter.write(c.phone+"\n");
+                fileWriter.write(c.password+"\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isValidEgyptianPhoneNumber(String phoneNumber) {
+        // Egypt phone number regex pattern
+        String regex = "^01[0-2]\\d{8}$";
+
+        // Create a pattern object
+        Pattern pattern = Pattern.compile(regex);
+
+        // Match the input string with the pattern
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        // Return true if the phone number matches the pattern, false otherwise
+        return matcher.matches();
+    }
+
+    public boolean isValidEmail(String email) {
+        // Email regex pattern
+        String regex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
+        // Create a pattern object
+        Pattern pattern = Pattern.compile(regex);
+
+        // Match the input string with the pattern
+        Matcher matcher = pattern.matcher(email);
+
+        // Return true if the email matches the pattern, false otherwise
+        return matcher.matches();
+    }
+
+    public void register(){
+        String FullName = new String(), Email = new String(), PhoneNumber = new String(), Pw = new String();
+        Scanner name = new Scanner(System.in);
+        System.out.print("Full name:");
+        FullName = name.nextLine();
+        while(true){
+            Scanner email = new Scanner(System.in);
+            System.out.print("Email:");
+            Email = email.nextLine();
+            if(isValidEmail(Email)){
+                break;
+            }
+            else{
+                int option;
+                Scanner myObj = new Scanner(System.in);
+                System.out.println("Wrong email format, click 1 to try again");
+                option = myObj.nextInt();
+                if (option != 1) {
+                    System.out.println("Registration failed");
+                    return;
+                }
+            }
+        }
+        while(true){
+            Scanner phone = new Scanner(System.in);
+            System.out.print("Phone number:");
+            PhoneNumber = phone.nextLine();
+            if(isValidEgyptianPhoneNumber(PhoneNumber)){
+                break;
+            }
+            else{
+                int option;
+                Scanner myObj = new Scanner(System.in);
+                System.out.println("Wrong phone number format, click 1 to try again");
+                option = myObj.nextInt();
+                if (option != 1) {
+                    System.out.println("Registration failed");
+                    return;
+                }
+            }
+        }
+        Scanner password = new Scanner(System.in);
+        System.out.print("Password:");
+        Pw = password.nextLine();
+
+        customer c = new customer(FullName, Email, PhoneNumber, Pw);
+        customers.add(c);
+
+        String file = "src/customer.txt";
+        clearfile(file);
+        write_to_file(file);
+    }
+
+    public void showcustomers(){
+        for(customer cu:customers){
+            cu.ShowData();
         }
     }
 }
