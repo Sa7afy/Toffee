59323e9e6f201f9e387af01e00d821f559bcff84
Yousef Abdalla <93826642+Sa7afy@users.noreply.github.com>
2023-05-11 08:45:57 +0300

    Add files via upload

---
+++ cart.java
@@ -0,0 +1,50 @@
+import java.util.Vector;
+import java.util.*;
+public class cart {
+    private Map<product, Integer> products;
+
+    public cart() {
+        products = new HashMap<product, Integer>();
+    }
+
+    public void add(product product, int quantity) {
+        if (products.containsKey(product)) {
+            products.put(product, products.get(product) + quantity);
+        } else {
+            products.put(product, quantity);
+        }
+    }
+
+    public void remove(product product, int quantity) {
+        if (products.containsKey(product)) {
+            int newQuantity = products.get(product) - quantity;
+            if (newQuantity <= 0) {
+                products.remove(product);
+            } else {
+                products.put(product, newQuantity);
+            }
+        }
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Map.Entry<product, Integer> entry : products.entrySet()) {
+            total += entry.getKey().getProductPrice() * entry.getValue();
+        }
+        return total;
+    }
+
+
+    public void print() {
+        System.out.println("Cart:");
+        for (Map.Entry<product, Integer> entry : products.entrySet()) {
+            System.out.println(entry.getKey().getProductName() + " x " + entry.getValue() + " @ $" + entry.getKey().getProductPrice() + " = $" + entry.getKey().getProductPrice() * entry.getValue());
+        }
+        System.out.println("Total: $" + calculateTotal());
+    }
+
+
+    public void view(){
+
+    }
+}
