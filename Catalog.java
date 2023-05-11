52823a924f30b68335491c200f7e972d9d42bff5
MohandMagdy <92790188+Mohandmagdy@users.noreply.github.com>
2023-05-11 09:01:29 +0300

    Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -1,32 +1,47 @@
 import java.util.Objects;
 import java.util.Vector;
 public class Catalog {
-    Vector<product> catalog;
+    Vector<category> catalog;
 
     public Catalog(){
-        catalog = new Vector<product>();
+        catalog = new Vector<category>();
     }
 
-    public void add(product prod){
-        catalog.add(prod);
+    public void add(category Category){
+        catalog.add(Category);
     }
 
-    public void delete(product prod){
-        catalog.remove(prod);
+    public void delete(category Category){
+        catalog.remove(Category);
     }
 
-    public void search(String id){
+    public void search_category(String id){
         for (int i = 0; i < catalog.size(); i++) {
-            if(Objects.equals(catalog.elementAt(i).ProductID, id)){
+            if(Objects.equals(catalog.elementAt(i).categoryID, id)){
                 catalog.elementAt(i).view();
                 return;
             }
         }
         System.out.println("Item not found");
     }
+
+    public void search_product(String id){
+        for (int i = 0; i < catalog.size(); i++) {
+            for (int j = 0; j < catalog.elementAt(i).products.size(); j++) {
+                if(Objects.equals(catalog.elementAt(i).products.elementAt(j), id)){
+                    catalog.elementAt(i).products.elementAt(j).view();
+                    return;
+                }
+            }
+        }
+        System.out.println("Item not found");
+    }
+
     public void view(){
-        for (product p : catalog) {
-            p.view();
+        for (category c : catalog) {
+            for (product p : c.products) {
+                p.view();
+            }
         }
     }
 }
