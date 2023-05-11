e58ae2c5aa92dcec1bd64e1a00a1f0a4ba4f1fd4
MohandMagdy <92790188+Mohandmagdy@users.noreply.github.com>
2023-05-12 00:23:03 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("welcome To Toffee Store");
         System.out.println("Made by Yousef Abdalla Attia(20210475) and Mohand Magdy Ahmed(20210412) ");
         Scanner in = new Scanner(System.in);
-        int option =  0;
+        int option = 0;
         while (toffee.curCustomer.ID == 1) {
             System.out.println("what you want to do now...?");
             System.out.println("1-Login\n2-register\n3-Browse catalog\n4-Exit");
@@ -24,56 +24,69 @@ public class Main {
                 return;
             }
         }
-        while (true){
+        while (true) {
             Scanner in2 = new Scanner(System.in);
             System.out.println("what do you want to do now...?");
-            System.out.println("1-Browse catalog\n2-show Cart\n3-checkout cart\n4-clear cart\n5-exit");
+            System.out.println("""
+                    1-Browse catalog
+                    2-show Cart
+                    3-checkout cart
+                    4-clear cart
+                    5-Remove item from the cart
+                    6-exit""");
             int op = in2.nextInt();
-            if (op ==1){
+            if (op == 1) {
                 toffee.catalog.view();
                 System.out.println("want add to cart? 1-YES 2-NO");
                 int want = in2.nextInt();
-                if(want ==1){
+                if (want == 1) {
                     System.out.print("Enter Product ID: ");
                     int id = in2.nextInt();
                     product now = toffee.catalog.search(id);
-                    if (now.ProductName!="NULL") {
+                    if (now.ProductName != "NULL") {
                         System.out.print("Enter Amount: ");
                         int amount = in2.nextInt();
-                        if (amount<=now.quantity){
-                            toffee.curCustomer.mycart.add(now,amount);
-                            now.quantity-=amount;
+                        if (amount <= now.quantity) {
+                            toffee.curCustomer.mycart.add(now, amount);
+                            now.quantity -= amount;
                             System.out.println("ADDED");
-                        }
-                        else {
+                        } else {
                             System.out.println("This quantity not available");
                         }
-                    }
-                    else{
+                    } else {
                         System.out.println("No such an ID");
                     }
                 }
-            }
-            else if (op == 2){
+            } else if (op == 2) {
                 toffee.curCustomer.mycart.view();
-            }
-            else if (op == 3){
-                if(Objects.equals(toffee.curCustomer.Email, "1")){
+            } else if (op == 3) {
+                if (Objects.equals(toffee.curCustomer.Email, "1")) {
                     System.out.println("You must be logged in.");
-                }
-                else{
+                } else {
                     Order o;
                     o = toffee.curCustomer.check_out();
                     toffee.curCustomer.show_an_order(o);
                 }
-            }
-            else if (op == 4) {
+            } else if (op == 4) {
                 toffee.curCustomer.mycart.clear();
-            }
-            else if (op == 5){
-
-            }
-            else break;
+            } else if (op == 5) {
+                toffee.curCustomer.mycart.view();
+                System.out.println("This is your items in the cart\n" +
+                        "Please enter ID and quantity of item you want to delete");
+                product prod;
+                int ID, quantity;
+                Scanner idd = new Scanner(System.in);
+                System.out.print("ID:");
+                ID = idd.nextInt();
+                System.out.print("Quantity:");
+                Scanner quant = new Scanner(System.in);
+                quantity = quant.nextInt();
+                prod = toffee.catalog.search(ID);
+                int removed = toffee.curCustomer.mycart.remove(prod, quantity);
+                prod.quantity += removed;
+            } else break;
         }
     }
 }
+
+
