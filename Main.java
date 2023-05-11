df6dc88a68f5f4e4deed3a24af81c0b0506a6c0d
Yousef Abdalla <93826642+Sa7afy@users.noreply.github.com>
2023-05-11 12:40:58 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        customer c1 = new customer("Yousef","yaballa463@gmail.com","01113129924","ABC");
-        customer c2 = new customer("Yousef","yaballa463@gmail.com","01113129924","ABC");
-        customer c3 = new customer("Yousef","yaballa463@gmail.com","01113129924","ABC");
-
-        c1.ShowData();
+        Toffee system = new Toffee();
+        system.ReadProducts();
+        system.ReadCustomers();
+        system.login();
     }
 }
