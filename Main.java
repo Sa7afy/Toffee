a6c85e20d6dd640e1bb90eab95a79ace7ca4437b
Yousef Abdalla <93826642+Sa7afy@users.noreply.github.com>
2023-05-12 00:51:16 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -43,7 +43,7 @@ public class Main {
                     System.out.print("Enter Product ID: ");
                     int id = in2.nextInt();
                     product now = toffee.catalog.search(id);
-                    if (now.ProductName != "NULL") {
+                    if (Objects.equals(now.ProductName, "NULL")) {
                         System.out.print("Enter Amount: ");
                         int amount = in2.nextInt();
                         if (amount <= now.quantity) {
@@ -63,8 +63,7 @@ public class Main {
                 if (Objects.equals(toffee.curCustomer.Email, "1")) {
                     System.out.println("You must be logged in.");
                 } else {
-                    Order o;
-                    o = toffee.curCustomer.check_out();
+                    toffee.curCustomer.check_out();
                 }
             } else if (op == 4) {
                 toffee.curCustomer.mycart.clear();
@@ -87,5 +86,6 @@ public class Main {
         }
     }
 }
+
 
 
